4d0fa19bc71926d58ac0bb8558e49d41f95c53d9
matthew.gerring <matthew.gerring@gmail.com>
2012-06-27 16:44:31 +0100

    Fix to http://jira.diamond.ac.uk/browse/DAWNSCI-85

--- org.dawb.passerelle.actors/src/org/dawb/passerelle/actors/ui/config/FieldComposite.java
+++ org.dawb.passerelle.actors/src/org/dawb/passerelle/actors/ui/config/FieldComposite.java
@@ -18,7 +18,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
