d746cafc5a2020fb5144c4e850e2f5142058a2b1
matthew.gerring <matthew.gerring@gmail.com>
2012-08-01 11:35:40 +0100

    Made filter case insensitive and added logging if filter took away all files.

--- org.dawb.passerelle.actors/src/org/dawb/passerelle/actors/data/DataImportSource.java
+++ org.dawb.passerelle.actors/src/org/dawb/passerelle/actors/data/DataImportSource.java
@@ -307,6 +307,10 @@ public class DataImportSource extends AbstractDataMessageSource implements IReso
 					}
 				}
 			}
+			
+			if (fileQueue!=null && fileQueue.isEmpty()) {
+				logger.info("No files found in '"+file.getAbsolutePath()+"'. Filter is set to: "+filterParam.getExpression());
+			}
 		}
 	}
 	
@@ -377,7 +381,7 @@ public class DataImportSource extends AbstractDataMessageSource implements IReso
     private boolean isRequiredFileName(String fileName) {
     	if (fileFilter==null || "".equals(fileFilter)) return true;
 		if (filterParam.isJustWildCard()) {
-			final StringMatcher matcher = new StringMatcher(fileFilter, false, false);
+			final StringMatcher matcher = new StringMatcher(fileFilter, true, false);
 		    return matcher.match(fileName);
 		} else {
 			return fileName.matches(fileFilter);
