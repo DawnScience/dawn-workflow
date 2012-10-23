8deb327aba8a6876c0a31ad214d56f0701811845
matthew.gerring <matthew.gerring@gmail.com>
2012-10-23 09:29:18 +0100

    Added handler for creating a workflow.

---
+++ org.dawb.passerelle.actors/src/org/dawb/passerelle/actors/ui/CreateMonitorWorkflowHandler.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2012 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dawb.passerelle.actors.ui;
+
+import java.io.File;
+
+import org.dawb.common.ui.util.EclipseUtils;
+import org.dawb.common.util.io.FileUtils;
+import org.dawb.common.util.io.IFileSelector;
+import org.dawb.passerelle.common.project.PasserelleNewProjectWizard;
+import org.dawb.passerelle.common.utils.ModelUtils;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+
+import com.isencia.passerelle.workbench.model.editor.ui.editor.PasserelleModelMultiPageEditor;
+
+public class CreateMonitorWorkflowHandler extends AbstractHandler {
+
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+        try {
+			final IFileSelector fileView   = (IFileSelector)EclipseUtils.getActivePage().getActivePart();	
+			File     selected   = fileView.getSelectedFile();
+			if (!selected.isDirectory()) selected = selected.getParentFile();
+			
+			// Get any workflow project or create one called 'workflows'
+			IProject project = EclipseUtils.getExistingProject("org.dawb.passerelle.common.PasserelleNature");
+			if (project == null) {
+				final PasserelleNewProjectWizard wizard = (PasserelleNewProjectWizard)EclipseUtils.openWizard("org.edna.passerelle.common.project.PasserelleWizard", true); // Quite old wizard id!
+				project = wizard.getProjectCreated();
+			}
+			
+			// Create workflow (moml) file
+			final String name    = "Monitor_"+selected.getName();
+			final File   toWrite = FileUtils.getUnique(selected, name, "moml");
+			final IFile  file    = ModelUtils.createFolderMonitorWorkflow(project, toWrite.getName(), selected, null);
+			
+			// Refresh project
+			project.refreshLocal(IResource.DEPTH_ONE, null);
+			
+			// Open file in editor
+			final PasserelleModelMultiPageEditor ed = (PasserelleModelMultiPageEditor)EclipseUtils.openEditor(file);
+			
+			// Go to edit tab
+			ed.setPasserelleEditorActive();
+			
+	        return Boolean.TRUE;
+        } catch (Exception ne) {
+        	throw new ExecutionException("Cannot open file navigator part!", ne);
+        }
+	}
+
+}
