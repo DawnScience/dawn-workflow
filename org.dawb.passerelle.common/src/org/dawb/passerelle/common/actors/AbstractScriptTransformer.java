be99613af40cf685eef9baecea5d4870da07513a
erwindl0 <erwindl0@gmail.com>
2014-04-02 17:43:49 +0200

    Fix for JIRA DAWNSCI-791 - Context menu on Actor does not match actor attributes
    - actor parameters must be public for correct cloning&copying

--- org.dawb.passerelle.common/src/org/dawb/passerelle/common/actors/AbstractScriptTransformer.java
+++ org.dawb.passerelle.common/src/org/dawb/passerelle/common/actors/AbstractScriptTransformer.java
@@ -61,7 +61,7 @@ public abstract class AbstractScriptTransformer extends AbstractPassModeTransfor
 	 */
 	private static Logger logger = LoggerFactory.getLogger(AbstractScriptTransformer.class);
 	
-	protected ResourceParameter   scriptFileParam;
+	public ResourceParameter   scriptFileParam;
 	protected String              scriptFilePath;
 	
 	private final List<DataMessageComponent> cache;
@@ -138,8 +138,8 @@ public abstract class AbstractScriptTransformer extends AbstractPassModeTransfor
 			
 			final DataMessageComponent despatch = getTransformedMessageInternal(cache);
 			if (despatch==null) return;
-	        sendOutputMsg(output, MessageUtils.getDataMessage(despatch, message));
-			
+	    
+			response.addOutputMessage(output, MessageUtils.getDataMessage(despatch, message));
 		} catch (DataMessageException dme) {
 			throw dme;
 		} catch (ProcessingException pe) {
@@ -151,7 +151,7 @@ public abstract class AbstractScriptTransformer extends AbstractPassModeTransfor
 
 	protected IResource getResource() throws Exception {
 		
-		String path = scriptFileParam.getExpression();
+		String path = scriptFileParam.stringValue();
 		IFile  file = null;
 		if (path==null || "".equals(path)) {
 			// Resource doesn't exist - create new file
