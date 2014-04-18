0c23d207f6f40b212e99c24f7e5f3bc701d421da
erwindl0 <erwindl0@gmail.com>
2014-04-18 10:56:40 +0200

    Enable activator for refactored o.d.p.actors.ui bundle & add an actor class provider in it.

--- org.dawb.passerelle.actors.ui/src/org/dawb/passerelle/actors/ui/Activator.java
+++ org.dawb.passerelle.actors.ui/src/org/dawb/passerelle/actors/ui/Activator.java
@@ -1,7 +1,25 @@
 package org.dawb.passerelle.actors.ui;
 
+import org.dawb.passerelle.actors.data.DataAppendTransformer;
+import org.dawb.passerelle.actors.data.DataChunkSource;
+import org.dawb.passerelle.actors.data.DataImportSource;
+import org.dawb.passerelle.actors.data.Expression;
+import org.dawb.passerelle.actors.data.FolderImportSource;
+import org.dawb.passerelle.actors.data.Scalar;
+import org.dawb.passerelle.actors.data.config.SliceParameter;
+import org.dawb.passerelle.actors.flow.ExpressionParameter;
+import org.dawb.passerelle.actors.flow.If;
+import org.dawb.passerelle.actors.flow.TimerSource;
+import org.dawb.passerelle.actors.net.TelnetTransformer;
+import org.dawb.passerelle.actors.roi.ROISource;
+import org.dawb.passerelle.actors.ui.config.FieldParameter;
+import org.dawb.passerelle.actors.ui.file.SubstituteTransformer;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import com.isencia.passerelle.ext.ModelElementClassProvider;
+import com.isencia.passerelle.ext.impl.DefaultModelElementClassProvider;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -13,6 +31,8 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
+
+  private ServiceRegistration<?> apSvcReg;
 	
 	/**
 	 * The constructor
@@ -24,9 +44,33 @@ public class Activator extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
+	@SuppressWarnings("unchecked")
+  public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+    apSvcReg = context.registerService(ModelElementClassProvider.class.getName(), 
+        new DefaultModelElementClassProvider(
+            DataAppendTransformer.class,
+            DataChunkSource.class,
+            DataImportSource.class,
+            Expression.class,
+            FolderImportSource.class,
+            Scalar.class,
+            SliceParameter.class,
+            ExpressionParameter.class,
+            If.class,
+            TimerSource.class,
+            org.dawb.passerelle.actors.ifdynaport.ExpressionParameter.class,
+            org.dawb.passerelle.actors.ifdynaport.If.class,
+            TelnetTransformer.class,
+            ROISource.class,
+            MessageSink.class,
+            UserInputSource.class,
+            UserModifyTransformer.class,
+            UserPlotTransformer.class,
+            FieldParameter.class,
+            SubstituteTransformer.class
+            ), null);
 	}
 
 	/*
@@ -34,6 +78,7 @@ public class Activator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
+    apSvcReg.unregister();
 		plugin = null;
 		super.stop(context);
 	}
