4c2cc05192923bd9820afe2f15a7336f825b6186
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-10-05 13:57:58 +0100

    remove unused import to reduce warnings count

--- org.dawb.workbench.jmx/src/org/dawb/workbench/jmx/py4j/Py4jWorkflowCallback.java
+++ org.dawb.workbench.jmx/src/org/dawb/workbench/jmx/py4j/Py4jWorkflowCallback.java
@@ -11,8 +11,6 @@ package org.dawb.workbench.jmx.py4j;
 
 import java.util.Map;
 
-import org.dawb.workbench.jmx.UserInputBean;
-
 public interface Py4jWorkflowCallback {
 	
 	public void setActorSelected(String actorName, boolean isSelected);
