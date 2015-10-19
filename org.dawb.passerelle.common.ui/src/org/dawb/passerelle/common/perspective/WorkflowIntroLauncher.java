14366d5e100d88b8ec61e051af06e94ec736a192
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-10-19 16:55:29 +0100

    extends from AbstractPerspectiveLaunch to track workflow perspective event

--- org.dawb.passerelle.common.ui/src/org/dawb/passerelle/common/perspective/WorkflowIntroLauncher.java
+++ org.dawb.passerelle.common.ui/src/org/dawb/passerelle/common/perspective/WorkflowIntroLauncher.java
@@ -8,36 +8,13 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */ 
 package org.dawb.passerelle.common.perspective;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
 
-public class WorkflowIntroLauncher implements IWorkbenchWindowActionDelegate {
+import org.dawb.common.ui.perspective.AbstractPerspectiveLaunch;
+
+public class WorkflowIntroLauncher extends AbstractPerspectiveLaunch {
 
 	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void init(IWorkbenchWindow window) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void run(IAction action) {
-		try {
-			PlatformUI.getWorkbench().showPerspective(WorkflowPerspective.ID,PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-		} catch (WorkbenchException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
+	public String getID() {
+		return WorkflowPerspective.ID;
 	}
 }
